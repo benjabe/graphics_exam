0e6313bad0d4ff808856d7656b27fcd5081e2d11
benjabe <benjaminbergzh@gmail.com>
2019-05-04 21:55:07 +0200

    Load raindrop model

--- graphics_exam/Raindrop.cpp
+++ graphics_exam/Raindrop.cpp
@@ -4,10 +4,11 @@
 
 Raindrop::Raindrop()
 {
-    m_shader = Shader("model_loading.vert", "model_loading.frag");
+    m_shader = Shader("model_loading.vert", "raindrop.frag");
     m_shader.use();
     m_shader.set_vec3("color", { 0.0f, 0.0f, 0.8f });
     m_position = glm::vec3(0.0f);
+    m_model = Model("Assets/Models/Raindrop/raindrop.obj");
 }
 
 
@@ -17,8 +18,6 @@ Raindrop::~Raindrop()
 
 void Raindrop::render(const glm::mat4 & projection, const glm::mat4 & view, const glm::vec3 & view_position, const DirectionalLight & directional_light, const std::vector<PointLight>& point_lights)
 {
-    glBindVertexArray(m_vao);
-
     // Set up shader
     m_shader.use();
     glm::mat4 model(1.0f);
